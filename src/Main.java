1fb401fb83cdbbd0f942f416070b9b2629f78f71
ashishbar404509 <wb-ashishbar404509@antfin.com>
2019-03-16 10:48:17 +0530

    first

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,15 @@ public class Main {
         admin.addUser(new User("Rohan", "M", 36, true, true));
         admin.addVehicle(new Car("Rohan", "Swift", "KA-01-12345", 4));
 
-        admin.addUser(new User("Rohan", "M", 36, true, true));
-        admin.addVehicle(new Car("Rohan", "Swift", "KA-01-12345", 4));
+        admin.addUser(new User("Shashank", "M", 29, true, true));
+        admin.addVehicle(new Car("Shashank", "Baleno", "TS-05-62395", 4));
 
+
+        admin.addUser(new User("Shashank", "M", 29, true, true));
+
+
+        admin.addUser(new User("Shipra", "F", 27, true, true));
+        admin.addVehicle(new Car("Shashank", "Baleno", "TS-05-62395", 4));
 
         print();
     }
